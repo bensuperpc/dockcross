e6c4a14d75d1ff43fb21154d0de7e8ea2c879cde
Bensuperpc <bensuperpc@gmail.com>
2021-08-26 17:57:42 +0200

    Fix test and add
    Fix test and add
    
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- tools/dockcross-command.sh
+++ tools/dockcross-command.sh
@@ -15,7 +15,7 @@ if (( $# >= 2 )); then
     chmod +x ./dockcross-"$image"
     
     echo "Run command in dockcross-$image"
-    ./dockcross-"$image" "$command"
+    ./dockcross-"$image" $command
 else
     echo "Usage: ${0##*/} <docker imag (ex: linux-x64/linux-x64-clang/linux-arm64/windows-shared-x64/windows-static-x64...)> <command>"
     exit 1
